be5f960b99bb9cc83a06b3c7f56bbe8feba0db3d
Aleksej Edin <shwed.berlin@gmail.com>
2017-07-17 13:13:50 +0200

    added .codebeatignore for documentation folder

--- Angular2webapp/Angular2webapp/app/core/action-menu/action-menu.component.ts
+++ Angular2webapp/Angular2webapp/app/core/action-menu/action-menu.component.ts
@@ -1,7 +1,6 @@
 ﻿import { Component } from '@angular/core';
 
 /**
- *
  * ## Action menu component ##
  */
 @Component({
--- Angular2webapp/Angular2webapp/app/core/app.storage.ts
+++ Angular2webapp/Angular2webapp/app/core/app.storage.ts
@@ -1,17 +1,30 @@
 ﻿import { Injectable } from '@angular/core';
 
+/**
+ * App storage service
+ */
 @Injectable()
 export class AppStorage {
     private instanceId: string;
 
+    /**
+     * Constructor
+     */
     constructor() {
         console.info("App storage constructor called");
     }
 
+    /**
+     * Set Instance Id
+     * @param instanceId
+     */
     setInstaceId(instanceId: string) {
         this.instanceId = instanceId;
     }
 
+    /**
+     * Get Instance Id
+     */
     getInstanceId(): string {
         if (!this.instanceId) return "- undef -";
         return this.instanceId;
